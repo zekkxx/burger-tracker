809f249dfd2c84ae0a3e34520fd48805e5859a37
Kieran 'zekkxx' Anthony <zekkxx@gmail.com>
2021-02-13 14:26:57 -0600

    Set up handlebars / route

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 let express = require("express");
 let exphbs = require("express-handlebars");
 
+let myBurgers=[
+    {
+        name: "Kieran's Special Flameburger",
+        devoured: false
+    },
+    {
+        name: "Fried Chicken McZeke",
+        devoured: false
+    },
+    {
+        name: "Penn Lake's Roast Beef",
+        devoured: false
+    },
+    {
+        name: "Triple Decker Pounder",
+        devoured: false
+    },
+    {
+        name: "French Toast Super Burger",
+        devoured: true
+    }
+]
+
 const PORT = process.env.PORT || 8080;
 let app = express();
 
@@ -10,6 +33,10 @@ app.use(express.json());
 
 app.engine("handlebars", exphbs({defaultLayout:"main"}));
 app.set("view engine", "handlebars");
+
+app.get("/", (req, res)=>{
+    res.render("index", { burgers: myBurgers })
+})
 
 app.listen(PORT, function(){
     console.log("Server listening on: http://localhost:"+PORT);
